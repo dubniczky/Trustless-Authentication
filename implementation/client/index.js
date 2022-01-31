0c501c1be25ac8d24566b4b4a2021404b25cf27a
HorvatKZ <krisztofer0605@gmail.com>
2022-01-31 18:34:47 +0100

    Server signup implementation

--- implementation/client/index.js
+++ implementation/client/index.js
@@ -2,22 +2,69 @@ unameInput = document.getElementById('username');
 passwdInput = document.getElementById('password');
 
 
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+    
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+function bytesToBase64(bytes) {
+    return btoa(ab2str(bytes))
+}
+
+
+function base64ToBytes(base64) {
+    return str2ab(atob(base64))
+}
+
+
 async function sendPasswd(type, context) {
-    hash = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str) + context.pubsalt)
-    cipher = await crypto.subtle.encrypt({name: "RSA-OAEP"}, context.pubkey, hash)
-    fetch(`/api/user/${type}/submit`, {
+    console.log(context)
+
+    const hash = await crypto.subtle.digest("SHA-256", new Uint8Array([
+            new TextEncoder("utf-8").encode(passwdInput.value),
+            base64ToBytes(context.pubSalt)
+    ]))
+
+    const pubKey = await crypto.subtle.importKey(
+        'spki',
+        base64ToBytes(context.pubKey),
+        { name: 'RSA-OAEP', hash: 'SHA-256' },
+        true,
+        ['encrypt']
+    )
+
+    const cipher = await crypto.subtle.encrypt({name: "RSA-OAEP"}, pubKey, hash)
+
+    fetch(`${location.href}api/user/${type}/submit`, {
         method: 'POST',
-        body: btoa(ab2str(cipher))
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            uname: unameInput.value,
+            passwd: bytesToBase64(cipher)
+        })
     })
 }
 
 
 function getContext(type) {
-    fetch(`/api/user/${type}/context`, {
+    fetch(`${location.href}api/user/${type}/context`, {
         method: 'POST',
-        body: unameInput.value
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ uname: unameInput.value })
     })
         .then(response => response.json())
         .then(context => sendPasswd(type, context))
-        .catch(console.error)
+        .catch(error => console.log(error))
 }
---
+++ implementation/server/index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const path = require('path')
+
+const cryptoUtils = require('./cryptoUtils')
+
+
+const app = express()
+
+// config
+const PORT = 8000
+const RSA_SIZE = 2048
+const SALT_SIZE = 18
+const PUB_EXP = new Uint8Array([1, 0, 1]);
+
+
+let sessions = {}
+let users = {}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', express.static(path.join(__dirname, '..', 'client')))
+
+
+app.post('/api/user/signup/context', async (req, res) => {
+    if (!req.body.uname) {
+        res.status(400).send('POST param uname is not defined')
+        return
+    }
+
+    const uname = req.body.uname
+    if (users[uname]) {
+        res.status(400).send(`User ${uname} has already registered`)
+        return
+    }
+
+    console.log(`Creating context for ${uname}`)
+
+    const { publicKey, privateKey } = cryptoUtils.genRSAKeys(RSA_SIZE, PUB_EXP)
+
+    const pubSalt = cryptoUtils.getBase64Salt(SALT_SIZE)
+    const privSalt = cryptoUtils.getBase64Salt(SALT_SIZE)
+
+    sessions[uname] = {
+        pubKey: publicKey,
+        privKey: privateKey
+    }
+
+    users[uname] = {
+        pubSalt: pubSalt,
+        privSalt: privSalt
+    }
+
+    res.send({
+        pubKey: cryptoUtils.RSAKeyToBase64(publicKey),
+        pubSalt: pubSalt
+    })
+})
+
+
+app.post('/api/user/signup/submit', async (req, res) => {
+    if (!req.body.uname || !req.body.passwd) {
+        res.status(400).send('POST param uname or passwd is not defined')
+        return
+    }
+
+    const uname = req.body.uname
+    const passwd = req.body.passwd
+    if (users[uname]) {
+        res.status(400).send(`User ${uname} has already registered`)
+        return
+    }
+    if (!sessions[uname]) {
+        res.status(400).send(`User ${uname} has no active context`)
+        return
+    }
+
+    console.log(`Registering ${uname}`)
+
+    const decrypted = cryptoUtils.decryptRSA(sessions[uname].privKey, base64ToBuffer(passwd))
+
+    users[uname].hash = cryptoUtils.hashAndSaltBase64(decrypted, base64ToBuffer(users[uname].privSalt))
+
+    console.log(`User ${uname} is registered with ${JSON.stringify(users[uname])}`)
+
+    res.send('Successful registration')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Ready on http://localhost:${PORT}`)
+})
